67003616a35d9ab83537a622807ab1c40c1a854e
mfsuzigan <mfsuzigan@gmail.com>
2025-11-14 02:48:43 -0300

    error handling

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
 function redirectPage() {
   const aliasLocation = document.querySelector(".c-tools-share__button.c-tools-share__button--copy-link")?.getAttribute("data-href");
 
-  if (aliasLocation) {
+  if (isValid(aliasLocation)) {
     console.log(`Location is ${aliasLocation}, redirecting to paywall remover...`);
     window.location.href = `https://isideload.com/?q=${aliasLocation}`;
+  }
+}
+
+function isValid(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
   }
 }
 
